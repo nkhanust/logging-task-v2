88ff0290a7955c96bf0015c17309512835a4cec8
Nabeel Khan <178791@ust-global.com>
2021-08-03 13:49:47 +0100

    completed. logging taks

--- logging task v2/Program.cs
+++ logging task v2/Program.cs
@@ -10,10 +10,25 @@ namespace logging_task_v2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+                                                                // Set up for Log
 
-            
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())       
+                .CreateLogger();
+            Log.Logger.Information("Starting now");
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<IGreetingService, GreetingService>();
+                })
+                .UseSerilog()
+                .Build();
+
+            var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
             List<Animal> List = new List<Animal>();
             Animal L = new Lion();
             Animal D = new Dog();
@@ -24,7 +39,32 @@ namespace logging_task_v2
             D.Print();
             L.Print();
 
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("D:\\UST Work\\Test.txt");
+                sw.WriteLine("Animal makes a sound");
+                sw.WriteLine("This is a dog, a domestic animal");
+                sw.WriteLine("This is a lion, a wild animal");
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("I think this works now");
+            }
 
+        }
+
+        static void BuildConfig(IConfigurationBuilder Builder)
+        {
+            Builder.SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+               .AddEnvironmentVariables();
         }
 
     }
